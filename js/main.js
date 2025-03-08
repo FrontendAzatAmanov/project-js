6640104f619ed32012da1dc1d55a94797a1a1da1
Azat <133127844+FrontendAzatAmanov@users.noreply.github.com>
2025-03-08 21:55:42 +0600

    add for and else js add

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 const numberOfFilms = Number(prompt('Сколько фильмов вы уже посмотрели?',''));
+if (numberOfFilms > 0 && numberOfFilms <= 10 ) {
+  console.log('Просмотрено довольно мало фильмов')
+}else if ( numberOfFilms > 10 && numberOfFilms <= 30) {
+  console.log('Вы классический зритель')
+}else if ( numberOfFilms > 30 ) {
+  console.log('Вы киноман!')
+}
+else (
+  console.error('error')
+)
+
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -7,12 +18,22 @@ const personalMovieDB = {
   genres: [],
   private: false,
 }
-let a = prompt('Один из последних просмотренных фильмов?');
-let b = +prompt('На сколько оцените его?');
-let c = prompt('Один из последних просмотренных фильмов?');
-let d = +prompt('На сколько оцените его?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i =0; i < 2;i++) {
+  let a = prompt('Один из последних просмотренных фильмов?');
+  let b = +prompt('На сколько оцените его?');
+
+  if (a != null && b != null && a != '' && b != '' && a.length < 50  ) {
+    personalMovieDB.movies[a] = b;
+    console.log('done')
+  }else {
+    console.log('error');
+    i--;
+  }
+
+}
+
+
 
 console.log(personalMovieDB);
